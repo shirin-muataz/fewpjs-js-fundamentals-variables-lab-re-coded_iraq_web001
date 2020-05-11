fbf5d70e7b5883d5780739dcc0ff7087da3ed0ce
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-11 08:10:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 const companyName='Scuber';
-const mostProfitableNeighborhood='Chelsea';
-const companyCeo = 'Susan Smith';
+let mostProfitableNeighborhood='Chelsea';
+let companyCeo = 'Susan Smith';
